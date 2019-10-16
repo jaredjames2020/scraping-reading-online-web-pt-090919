19a0c2086d69725424fab9231de516f8de2e082c
Jared S <jaredjames2020@gmail.com>
2019-10-16 19:54:54 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,4 +4,5 @@ require 'open-uri'
 html = open("https://flatironschool.com/")
 
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
-doc.css(".headline-26OIBN").text
+puts doc.css(".headline-26OIBN").text
+
